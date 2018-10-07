af7c04fcd6775cb5b9a39a6ecbafe6d5552614fe
Nicola Centrone <nicola.centrone.it@gmail.com>
2018-10-07 17:09:43 +0200

    feat: Implemented cat selection

--- js/app.js
+++ js/app.js
@@ -9,6 +9,10 @@ Cat.prototype.setPictureUrl = function (n) {
   this.imageUrl = url+(n+1);
 }
 
+Cat.prototype.increaseClick = function () {
+this.clicks += 1;
+}
+
 /* Functions */
 
 function setCatsUrl () {
@@ -23,17 +27,75 @@ function setCatsUrl () {
 
 function setImages (allCats) {
   let bar = document.querySelector('.select-bar');
+  let i = 1;
   allCats.forEach((el) => {
     let img = document.createElement('img');
-    img.src = el.imageUrl + ".jpg";
-    img.className = "thumb";
+    img.src = el.imageUrl + '.jpg';
+    img.className = 'thumb' + ' ' + 'cat' + i;
     bar.appendChild(img);
+    i++;
   });
 }
 
+function select () {
+  let images = document.querySelectorAll('img');
+  let clear = true;
+  images.forEach((el) => {
+    el.addEventListener('click', () => {
+      if (clear === false) {
+        document.querySelector('.clicks-area img').remove();
+        document.querySelector('.clicks-area h3').remove();
+        clear = true;
+      }
+        let src = el.src;
+        nameTheCats(allCats);
+        let newImg = document.createElement('img');
+        newImg.className = 'cat-pic';
+        newImg.src = src;
+        document.querySelector('.clicks-area').appendChild(newImg);
+        showClicks(allCats);
+        clear = false;
+        let target = event.target;
+        let nameSpace = document.querySelector('h3');
+        if (target.className === 'thumb cat1') {
+          nameSpace.innerHTML = allCats[0].name;
+        } else if (target.className === 'thumb cat2') {
+          nameSpace.innerHTML = allCats[1].name;
+        } else if (target.className === 'thumb cat3') {
+          nameSpace.innerHTML = allCats[2].name;
+        } else if (target.className === 'thumb cat4') {
+          nameSpace.innerHTML = allCats[3].name;
+        } else if (target.className === 'thumb cat5') {
+          nameSpace.innerHTML = allCats[4].name;
+        }
+    }, false);
+  });
+}
+
+function showClicks (allCats) {
+  let clicksNum = document.createElement('h3');
+  document.querySelector('.clicks-area').appendChild(clicksNum);
+  allCats.forEach((el) => {
+    clicksNum.innerHTML = el.clicks;
+  }, false);
+}
+
+function nameTheCats (allCats) {
+  const catName = ['Shomi', 'Shobi', 'Bim', 'Bum', 'Bam'];
+  let i = 0;
+  let imgs = document.querySelectorAll('img');
+  allCats.forEach((el) => {
+      el.name = catName[i];
+      i++;
+  }, false);
+
+}
+
 let allCats = setCatsUrl();
-debugger;
 setImages(allCats);
+select();
+
+
 // function click () {
 //   let count = 0;
 //
@@ -62,19 +124,7 @@ setImages(allCats);
 //
 // }
 //
-// function catsName () {
-//   const catName1 = 'Shimi';
-//   const catName2 = 'Shomi';
-//   const catName2 = 'Shobi';
-//   const catName2 = 'Shaggy';
-//   const catName2 = 'Shinobi';
-//
-//   let titles = document.querySelectorAll('h3');
-//   for (var i = 0; i < titles.length; i++) {
-//     titles[i].innerHTML = catName[i+1];
-//   }
-// }
-//
+
+
 // let myVar = click();
 // myVar();
-// catsName();
