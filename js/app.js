38d6d8f46fab32bb21e3a5daa105510638a2ba6d
Nicola Centrone <nicola.centrone.it@gmail.com>
2018-10-07 20:39:14 +0200

    docs: Run ESlint-Google

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
-var Cat = function (url, name) {
-    this.imageUrl = url;
-    this.name = name;
-    this.clicks = 0;
+let Cat = function(url, name) {
+  this.imageUrl = url;
+  this.name = name;
+  this.clicks = 0;
 };
 
-Cat.prototype.setPictureUrl = function (n) {
-  const url = "img/cat-pic";
+Cat.prototype.setPictureUrl = function(n) {
+  const url = 'img/cat-pic';
   this.imageUrl = url+(n+1);
-}
+};
 
-Cat.prototype.increaseClick = function () {
-this.clicks += 1;
-}
+Cat.prototype.increaseClick = function() {
+  this.clicks += 1;
+};
 
-/* Functions */
-
-function setCatsUrl () {
+/**
+* @description Instantiates 5 cat objects and push them into an array
+* @return {array} An array of objects containing 5 cats objects
+**/
+function setCatsUrl() {
   let catArray = [];
-  for (let i = 0; i < 5 ; i++) {
+  for (let i = 0; i < 5; i++) {
     let cat = new Cat();
     catArray.push(cat);
     cat.setPictureUrl(i);
@@ -25,7 +27,11 @@ function setCatsUrl () {
   return catArray;
 }
 
-function setImages (allCats) {
+/**
+* @description Creates an image in the DOM for every object in the allCats array
+* @param {array} allCats The array of cat objects
+**/
+function setImages(allCats) {
   let bar = document.querySelector('.select-bar');
   let i = 1;
   allCats.forEach((el) => {
@@ -37,7 +43,11 @@ function setImages (allCats) {
   });
 }
 
-function select () {
+/**
+* @description
+* @param {array} allCats The array of cat objects
+**/
+function select() {
   let images = document.querySelectorAll('img');
   let clear = true;
   images.forEach((el) => {
@@ -47,32 +57,36 @@ function select () {
         document.querySelector('.clicks-area h3').remove();
         clear = true;
       }
-        let src = el.src;
-        nameTheCats(allCats);
-        let newImg = document.createElement('img');
-        newImg.className = 'cat-pic';
-        newImg.src = src;
-        document.querySelector('.clicks-area').appendChild(newImg);
-        showClicks(allCats);
-        clear = false;
-        let target = event.target;
-        let nameSpace = document.querySelector('h3');
-        if (target.className === 'thumb cat1') {
-          nameSpace.innerHTML = allCats[0].name;
-        } else if (target.className === 'thumb cat2') {
-          nameSpace.innerHTML = allCats[1].name;
-        } else if (target.className === 'thumb cat3') {
-          nameSpace.innerHTML = allCats[2].name;
-        } else if (target.className === 'thumb cat4') {
-          nameSpace.innerHTML = allCats[3].name;
-        } else if (target.className === 'thumb cat5') {
-          nameSpace.innerHTML = allCats[4].name;
-        }
+      let src = el.src;
+      nameTheCats(allCats);
+      let newImg = document.createElement('img');
+      newImg.className = 'cat-pic';
+      newImg.src = src;
+      document.querySelector('.clicks-area').appendChild(newImg);
+      showClicks(allCats);
+      clear = false;
+      let target = event.target;
+      let nameSpace = document.querySelector('h3');
+      if (target.className === 'thumb cat1') {
+        nameSpace.innerHTML = allCats[0].name;
+      } else if (target.className === 'thumb cat2') {
+        nameSpace.innerHTML = allCats[1].name;
+      } else if (target.className === 'thumb cat3') {
+        nameSpace.innerHTML = allCats[2].name;
+      } else if (target.className === 'thumb cat4') {
+        nameSpace.innerHTML = allCats[3].name;
+      } else if (target.className === 'thumb cat5') {
+        nameSpace.innerHTML = allCats[4].name;
+      }
     }, false);
   });
 }
 
-function showClicks (allCats) {
+/**
+* @description Creates the number of clicks
+* @param {array} allCats The array of cat objects
+**/
+function showClicks(allCats) {
   let clicksNum = document.createElement('h3');
   document.querySelector('.clicks-area').appendChild(clicksNum);
   allCats.forEach((el) => {
@@ -80,51 +94,19 @@ function showClicks (allCats) {
   }, false);
 }
 
-function nameTheCats (allCats) {
+/**
+* @description Gives the attribute name to all the cats objects
+* @param {array} allCats The array of cat objects
+**/
+function nameTheCats(allCats) {
   const catName = ['Shomi', 'Shobi', 'Bim', 'Bum', 'Bam'];
   let i = 0;
-  let imgs = document.querySelectorAll('img');
   allCats.forEach((el) => {
-      el.name = catName[i];
-      i++;
+    el.name = catName[i];
+    i++;
   }, false);
-
 }
 
 let allCats = setCatsUrl();
 setImages(allCats);
 select();
-
-
-// function click () {
-//   let count = 0;
-//
-//   return () => {
-//     let catPic = document.querySelector(".cat-pic");
-//     let catPic2 = document.querySelector(".cat-pic2");
-//
-//     catPic.addEventListener('click', () => {
-//       let counter = document.querySelector("span");
-//       counter.remove();
-//       let newCounter = document.createElement("span");
-//       count++;
-//       newCounter.innerHTML = count;
-//       document.querySelector(".number-area").appendChild(newCounter);
-//     }, false);
-//
-//     catPic2.addEventListener('click', () => {
-//       let counter = document.querySelector("span");
-//       counter.remove();
-//       let newCounter = document.createElement("span");
-//       count++;
-//       newCounter.innerHTML = count;
-//       document.querySelector(".number-area").appendChild(newCounter);
-//     }, false);
-//   };
-//
-// }
-//
-
-
-// let myVar = click();
-// myVar();
