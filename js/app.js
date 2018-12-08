6168062b010a742f54a4695504910eb08c8e1709
Nicola Centrone <nicola.centrone.it@gmail.com>
2018-12-08 16:58:37 +0100

    docs: Update functions documentation

--- js/app.js
+++ js/app.js
@@ -10,8 +10,7 @@ $(function() {
   };
 
   /**
-  * @description cats is the array container for the cat objects. increaseClicks
-  * is the prototype method associated to every cat object.
+  * @description Cats is the array container for the cat objects.
   * @prototype
   */
   let model = {
@@ -26,35 +25,90 @@ $(function() {
   */
   let octopus = {
 
-    /* returns the array of objects cats, because the view can't access it
-    directly */
+    /**
+    * @description OCTOPUS Interface function for the view to access the
+    * model.
+    * @return {array} The array of cats.
+    */
     getCats: function() {
       return model.cats;
     },
 
+    /**
+    * @description OCTOPUS Interface function for the view to access the model.
+    * @return {object} The current cat stored in the attribute of the model
+    * object
+    */
     getCurrentCat: function() {
       return model.currentCat;
     },
 
-    /* returns the increaseClicks function, because the view can't access it
-    directly */
+    /**
+    * @description OCTOPUS Interface function for the view to access the model.
+    * Increments the clicks for the cat object in the parameter.
+    * @param {obj} cat - The current cat object
+    * @return {number} - The updated number of clicks
+    */
     getIncreasedClicks: function(cat) {
       return cat.clicks++;
     },
 
+    /**
+    * @description OCTOPUS Interface function for the view to access the model.
+    * @return {boolean} The attribute adminMode states if the Admin button is
+    * displayed.
+    */
     getAdminMode: function() {
       return model.adminMode;
     },
 
+    /**
+    * @description OCTOPUS Interface function for the view to write in the model
+    * Sets the cat object in the parameter into the currentCat attribute.
+    * @param {obj} catCopy - The cat it's been clicked in the cat list.
+    */
     setCurrentCat: function(catCopy) {
-      return model.currentCat = catCopy;
+      model.currentCat = catCopy;
     },
 
+    /**
+    * @description OCTOPUS Interface function for the view to write in the model
+    * Sets the new state of admin boolean variable to the model.adminMode
+    * attribute.
+    * @param {boolean} mode - A boolean to control the state of the Admin button
+    */
     setAdminMode: function(mode) {
-      return model.adminMode = mode;
+      model.adminMode = mode;
     },
 
-    /* kickstarts the code */
+    /**
+    * @description OCTOPUS Interface function for the view to write in the model
+    * set the currentCat's name with the name of the text parameter.
+    * @param {string} input - Contains the new name wrote in the input.
+    */
+    setCatName: function(input) {
+      model.currentCat.name = input;
+    },
+
+    /**
+    * @description OCTOPUS Interface function for the view to write in the model
+    * @param {string} input - Contains the new url wrote in the input.
+    */
+    setCatUrl: function(input) {
+      model.currentCat.imageUrl = input;
+    },
+
+    /**
+    * @description OCTOPUS Interface function for the view to write in the model
+    * @param {string} input - Contains the new click counter wrote in the input.
+    */
+    setCatClicks: function(input) {
+      model.currentCat.clicks = input;
+    },
+
+    /**
+    * @description Kickstarts the code
+    */
     init: function() {
       this.createCatObjects();
       this.setPictureUrl();
@@ -63,7 +117,9 @@ $(function() {
       view.addListenersToNames();
     },
 
-    /* just creates 5 cat objects and push them into the model.cats array */
+    /**
+    * @description Creates 5 cat objects and push them into the model.cats array
+    */
     createCatObjects: function() {
       for (let i = 0; i < 5; i++) {
         let cat = new Cat();
@@ -71,7 +127,9 @@ $(function() {
       }
     },
 
-    /* associates the url for every cat object */
+    /**
+    * @description Associates the url for every cat object.
+    */
     setPictureUrl: function() {
       let i = 1;
       model.cats.forEach((el) => {
@@ -81,7 +139,9 @@ $(function() {
       }, false);
     },
 
-    /* associates a name for every cat object */
+    /**
+    * @description: Sets a name for every cat object
+    */
     giveCatsName: function() {
       const catNames = ['Shomi', 'Shobi', 'Bim', 'Bum', 'Bam'];
       let i = 0;
@@ -90,24 +150,15 @@ $(function() {
         i++;
       }, false);
     },
-
-    setCatName: function(input) {
-      model.currentCat.name = input;
-    },
-
-    setCatUrl: function(input) {
-      model.currentCat.imageUrl = input;
-    },
-
-    setCatClicks: function(input) {
-      model.currentCat.clicks = input;
-    },
   };
 
   /* behaves like a DOM updater, it can't access the model directly */
   let view = {
 
-    /* takes every ul in the DOM and sticks a click event listener to it */
+    /**
+    * @description Takes every ul in the DOM and sticks a click event listener
+    * to it.
+    */
     addListenersToNames: function() {
       let clear = true;
       let init = true;
@@ -116,25 +167,28 @@ $(function() {
       let cat;
       for (let i = 0; i < cats.length; i++) {
         cat = cats[i];
+        /* Use of a closure to grab the object of the clicked cat */
         uls[i].addEventListener('click', (function(catCopy) {
           return function() {
             octopus.setCurrentCat(catCopy);
-            /* we don't want to clear the screen if it's the first time we
+            /* We don't want to clear the screen if it's the first time we
             render it */
             if (clear === false) {
               view.clear();
             }
-            /* it starts the first time you run the app. We don't want to create
+            /* Starts the first time you run the app. We don't want to create
             the DOM structure every time. Because the structure stays the same
             but only the contents gets cleaned */
             if (init === true) {
               view.renderInit(); // ok, make the structure
               view.clicker(); // add the listeners
             }
-            init = false;
+            init = false; // the init is complete
 
+            /* Starts to render the right info for the ul clicked */
             view.render();
-            //  starts to render the right info for the ul you clicked
+
+            /* Is the admin panel open? Let's get its status. */
             let admin = octopus.getAdminMode();
             if (admin === false) {
               view.renderControlsInit();
@@ -147,6 +201,9 @@ $(function() {
       };
     },
 
+    /**
+    * @description Renders the column of the left with the names of the cats.
+    */
     renderSelectBarInit: function() {
       let bar = $('.select-bar');
       let i = 1;
@@ -161,7 +218,9 @@ $(function() {
       });
     },
 
-    /* prints the list of cat names (ul) and adds the class names */
+    /**
+    * @description Prints the list of cat names (ul) and adds the class names
+    */
     renderSelectBar: function() {
       let i = 0;
       let cats = octopus.getCats();
@@ -172,13 +231,18 @@ $(function() {
       });
     },
 
+    /**
+    * @description Clears the clicks-area's content.
+    */
     clear: function() {
       $('.clicks-area h3').text('');
       $('.cat-pic').attr('src', '');
     },
 
-    /* we need to create the DOM structure only once. These elements won't be
-    .removed() but the content will be cleared with '' */
+    /**
+    * @description We need to create the DOM structure only once.
+    * These elements won't be .removed() but the content will be cleared with ''
+    */
     renderInit: function() {
       let clicksArea = $('.clicks-area');
       clicksArea.append('<h3 class=cats-name></h3>');
@@ -186,7 +250,9 @@ $(function() {
       clicksArea.append('<h3 class="clicks"></h3>');
     },
 
-    /* render the right info of the target cat*/
+    /**
+    * @description Refresh the page with the correct info of the target cat.
+    */
     render: function() {
       let cat = octopus.getCurrentCat();
       let nameSpace = $('.cats-name');
@@ -199,16 +265,25 @@ $(function() {
       catPic.attr('class', 'cat-pic');
     },
 
-    /* increases the clicks of the target cat */
+    /**
+    * @description Add the event listener for the cat's pic that will
+    * increment the counter when it's clicked.
+    */
     clicker: function() {
       $('.cat-pic').on('click', function(e) {
         let cat = octopus.getCurrentCat();
         cat.clicks ++;
         let clicksArea = $('.clicks');
+        // refresh the text immediately
         clicksArea.text(cat.clicks);
       });
     },
 
+    /**
+    * @description Renders the admin controls if they are not displayed.
+    * (e.g. the first time you run the app). Then updates the input values and
+    * adds the event listeners for the save and cancel buttons.
+    */
     renderControlsInit: function() {
       let cat = octopus.getCurrentCat();
       $('.b-controls__button').on('click', function(e) {
@@ -260,6 +335,9 @@ $(function() {
       });
     },
 
+    /**
+    * @description Updates the input fields with the current cat's values.
+    */
     renderControls: function() {
       let cat = octopus.getCurrentCat();
       let name = $('.b-controls__input-name');
@@ -271,6 +349,9 @@ $(function() {
       url.val(cat.imageUrl);
     },
 
+    /**
+    * @description Hides the admin control panel.
+    */
     hideControls: function() {
       let name = $('.b-controls__input-name');
       let url = $('.b-controls__input-url');
@@ -285,6 +366,9 @@ $(function() {
       cancel.attr('class', 'b-controls__button-cancel--hidden');
     },
 
+    /**
+    * @description Clear the cats names list.
+    */
     clearSelectBar: function() {
       let uls = $('ul');
       uls.text('');
